a794c8449073b595d5ea57f2a9bad3d4d6145892
Nisema <nisema13@gmail.com>
2026-03-17 17:30:48 -0500

    feat: Person class constructor

---
+++ main.py
@@ -0,0 +1,12 @@
+class Person():
+    def __init__(self,name: str, is_literate: bool, years_in_debt: int):
+        self.name = name
+        self.checking_balance = 0
+        self.savings_balance = 5000
+        self.debt = 30100
+        self.loan = 0
+        self.is_literate = is_literate
+        self.years_in_debt = 0
+        self.amount_
+
+        #Complete still
