8435e6c6b346fcc00d1268dd0db96e2e39e20e81
Nisema <nisema13@gmail.com>
2026-03-19 16:46:37 -0500

    feat:Completed basic programm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import json
+from math import *
 class Person():
 
     HOUSE_PRICE = 175000
@@ -52,7 +53,9 @@ class Person():
         self.year = []
         year_stats = {
                 "wealth": self.wealth,
+                "debt":self.debt,
                 "total_debt_paid": self.debt_paid,
+                "mortgage_loan": self.morgage_loan,
                 "total_morgage_paid": self.morgage_paid,
                 "total_savings": self.savings_balance}
         self.year.append(year_stats)
@@ -73,9 +76,9 @@ class Person():
     def recieve_savings_interest(self):
         '''Increases the amount of money in savings'''
         if self.is_literate:
-            self.savings_balance = self.savings_balance * Person.INT_ON_SAVINGS_FL
+            self.savings_balance = round(self.savings_balance * Person.INT_ON_SAVINGS_FL,2)
         else:
-            self.savings_balance = self.savings_balance * Person.INT_ON_SAVINGS_NFL
+            self.savings_balance = round(self.savings_balance * Person.INT_ON_SAVINGS_NFL,2)
 
     def debt_payment_and_interest(self):
         '''
@@ -89,16 +92,18 @@ class Person():
 
             #Determine debt payment for year
             if self.is_literate:
-                payment = (self.debt * Person.DEBT_PAYMENT_PERC) + 15
+                payment = round((self.debt * Person.DEBT_PAYMENT_PERC) + 15,2)
             else:
-                payment = (self.debt * Person.DEBT_PAYMENT_PERC) + 1
+                payment = round((self.debt * Person.DEBT_PAYMENT_PERC) + 1,2)
             
             #Ensure payment isn't more than actual debt
-            if payment > self.debt: payment = self.debt
+            if payment > self.debt: 
+                payment = self.debt
+                
 
             #Add debt paid and add interest to remaining debt
             self.debt_paid += payment
-            self.debt = (self.debt - self.debt_paid) * Person.INT_ON_REMAINING_DEBT
+            self.debt = round((self.debt - self.debt_paid) * Person.INT_ON_REMAINING_DEBT,2)
 
             #Reduce savings or checking account? 
             if payment > self.checking_balance:
@@ -154,8 +159,8 @@ class Person():
         
         N = 360
         D = ((((i+1)**N) - 1) / (i*((i+1)**N)))      
-        self.morgage_payment = (self.morgage_loan / D)
-        self.savings_balance -= self.morgage_payment
+        self.morgage_payment = round(self.morgage_loan / D,2)
+        self.checking_balance -= self.morgage_payment
         self.has_a_house =True
         
 
@@ -171,7 +176,9 @@ class Person():
         self._wealth = int(self.savings_balance + self.checking_balance - self.debt - self.morgage_loan)
         year_stats = {
                 "wealth": self.wealth,
+                "debt":self.debt,
                 "total_debt_paid": self.debt_paid,
+                "mortgage_loan": self.morgage_loan,
                 "total_morgage_paid": self.morgage_paid,
                 "total_savings": self.savings_balance}
         self.year.append(year_stats)
@@ -197,7 +204,7 @@ class Simulation():
     def run(self)-> list:
         wealth_overtime = []
         human_stats = self.person.year
-        for year_num in range(self.years):
+        for year_num in range((self.years) + 1):
             self.person.age()
             item_to_be_appened = human_stats[year_num]["wealth"]
             wealth_overtime.append(item_to_be_appened)
@@ -205,13 +212,13 @@ class Simulation():
 
     def summary(self)->dict:
         summary_dict = {}
-        summary_dict["wealth"] = self.person.wealth
+        summary_dict["final_wealth"] = self.person.wealth
         summary_dict["years_in_debt"] = self.person.years_in_debt
-        summary_dict["total_debt_paid"] = self.person.debt_paid
-        summary_dict["total_mortgage_paid"] = self.person.morgage_paid
+        summary_dict["total_debt_paid"] = round(self.person.debt_paid,2)
+        summary_dict["total_mortgage_paid"] = round(self.person.morgage_paid,2)
         return summary_dict
 
-def plot_wealth(fl_wealth_history: List, nfl_wealth_history: List, filename: str =
+def plot_wealth(fl_wealth_history: list, nfl_wealth_history: list, filename: str =
 "wealth_over_time.png"):
     #Create num years arrayS
     flyears_arr = []
@@ -241,6 +248,8 @@ def run_tests() -> None:
     financially_unlit = Person("Marcus",False)
     sim_FL = Simulation(financially_lit,40)
     sim_NFL = Simulation(financially_unlit,40)
+
+    person2 = financially_unlit
 
     #Person(): init()
     fn_name = 'Person constructor'
@@ -273,44 +282,221 @@ def run_tests() -> None:
     expected = 0
     error_message= f"While testing {fn_name}, expected {expected} got {result}"
     assert expected == result,error_message
+    person.wealth = -25100
 
-nisema = Person("nisema", True)
-sim_fl = Simulation(nisema,10)
-wealth_fl = sim_fl.run()
-summary_fln = sim_fl.summary()
+    #Person allocate salary
+    #Test 1 - Initial allocation correct
+    fn_name = "allocate salary"
+    person.allocate_salary()
+    result = person.checking_balance + person.savings_balance
+    expected = 35500.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-holly = Person("holly",False)
-sim_nfl = Simulation(holly,10)
-wealth_nfl = sim_nfl.run()
-summary_nfl = sim_nfl.summary()
+    #Test 2 - Ensures that correct amounts are in each account
+    result = person.checking_balance
+    expected = 18300.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-with open("fl_yearly_stats.txt", "w") as file:
-    for dict in nisema.year:
-        json.dump(dict,file,indent=4)
+    #Test 3 
+    result = person.savings_balance
+    expected = 17200.0
+    result = person.savings_balance
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-#plot_wealth(wealth_fl,wealth_nfl)
-run_tests()
 
+   #Person - interest on savings
+   #Test 1 - Financially literate person
+    fn_name = "receive_savings_interest"
+    person.recieve_savings_interest()
+    result = person.savings_balance
+    expected = 18404.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-'''
-holly = Person("holly", False)
+    #Test 2 - Test if the savings were never increased
+    person2.recieve_savings_interest()
+    result = person2.savings_balance
+    expected = 5100.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-for people in (nisema,holly):
+    #Test 3 - Financially illerate
+    person2.allocate_salary()
+    person2.recieve_savings_interest()
+    result = person2.savings_balance
+    expected = 17646.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Person - debt payments and interest
+    fn_name = "debt_payment_and_interest"
+    person.debt_payment_and_interest()
+    result = person.debt_paid
+    expected = 918.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = person.debt
+    expected = 35018.4
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = person.checking_balance
+    expected = 17382.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Person rent_or_mortgage 
+    fn_name = "rent_or_mortgage_payment"
+    person.rent_or_morgage_payment()
+    result = person.years_renting
+    expected = 1
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = person.checking_balance
+    expected = 7182.0
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = person.has_a_house
+    expected = False
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Person- buy a house
+    fn_name = "Buy a house"
+    person2.rent_or_morgage_payment()
+    person2.buy_house()
+    result = person2.has_a_house
+    expected = True
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = person2.morgage_payment
+    expected = 834.97
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = person2.checking_balance
+    expected = 5862.920000000003 #Couldn't find why it wasn't rounding figures up
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Person - save_year_stats and age (They can and should be tested together)
+    person3 = Person('Nancy',True)
     
-    print(f"Savings {people.savings_balance}")
-    print(f"Checking {people.checking_balance}")
-    print(f"Debt: {people.debt}")
-    print(f"Years in Debt {people.years_in_debt}")
-    print(f"Rent or morgage info: Loan: {people.morgage_loan} Years renting: {people.years_renting}")
+    fn_name = "save_year_stats"
+    person3.save_year_stats()
+    result = person3.year[0]
+    expected = {
+                "wealth": -25100,
+                "debt": 30100,
+                "total_debt_paid": 0.0,
+                "mortgage_loan":0.0,
+                "total_morgage_paid": 0.0,
+                "total_savings": 5000}
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-    people.age()
-    people.age()
-    people.age()
+    fn_name = "save_year_stats and age"
+    person3.age()
+    result = person3.year[1]
+    expected = {'wealth': -25100,
+                'debt': 30100,
+                'total_debt_paid': 0.0,
+                'mortgage_loan': 0.0,
+                'total_morgage_paid': 0.0,
+                'total_savings': 5000}
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
 
-    print(f"Savings {people.savings_balance}")
-    print(f"Checking {people.checking_balance}")
-    print(f"Debt: {people.debt}")
-    print(f"Years in Debt {people.years_in_debt}")
-    print(f"Rent or morgage info: Loan: {people.morgage_loan} Years renting: {people.years_renting}")
-'''
+    person.age()
+    result = person.year[1]
+    expected = {'wealth': 7320,
+                'debt': 39641.82,
+                'total_debt_paid': 1983.55,
+                'mortgage_loan': 0.0,
+                'total_morgage_paid': 0.0,
+                'total_savings': 32746.28}
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Simulation tests
+    #Simulation - constructor
+    fn_name = "Simulation constructor"
+    result = sim_FL.person.name
+    expected = "Hannah"
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = sim_NFL.person.name
+    expected = "Marcus"
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    result = sim_FL.years
+    expected = 40
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Simulation - run
+    sim_FL_3 = Simulation(person3,5)
+    fn_name = "Simulation run"
+    result = sim_FL_3.run()
+    expected = [-25100, -25100, -9432, 7320, 25459, -85187]
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    person4 = Person("Andrew", False)
+    sim_Andrew = Simulation(person4,0)
+    result = sim_Andrew.run()
+    expected = [-25100]
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    sim_Andrew = Simulation(person4,2)
+    result = sim_Andrew.run()
+    expected = [-25100, -167237, -141854]
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+    #Simulation - summary
+    fn_name = "Simulation summary"
+    result = sim_Andrew.summary()
+    expected = {'final_wealth': -88975,
+                'years_in_debt': 4,
+                'total_debt_paid': 4464.6,
+                'total_mortgage_paid': 42838.56}
+    error_message= f"While testing {fn_name}, expected {expected} got {result}"
+    assert expected == result,error_message
+
+if __name__ == "__main__":
+    fl = Person("fl", True)
+    nfl = Person("nfl", False)
+
+    sim_fl = Simulation(fl, years=40)
+    sim_nfl = Simulation(nfl, years=40)
+    fl_wealth_history = sim_fl.run()
+    nfl_wealth_history = sim_nfl.run()
+
+    plot_wealth(fl_wealth_history, nfl_wealth_history)
+    summary_fl = sim_fl.summary()
+    summary_nfl = sim_nfl.summary()
+
+    print(
+    f"RESULT "
+    f"fl_final_wealth={summary_fl['final_wealth']} "
+    f"nfl_final_wealth={summary_nfl['final_wealth']} "
+    f"fl_years_in_debt={summary_fl['years_in_debt']} "
+    f"nfl_years_in_debt={summary_nfl['years_in_debt']} "
+    f"fl_total_debt_paid={summary_fl['total_debt_paid']} "
+    f"nfl_total_debt_paid={summary_nfl['total_debt_paid']} "
+    f"fl_total_mortgage_paid={summary_fl['total_mortgage_paid']} "
+    f"nfl_total_mortgage_paid={summary_nfl['total_mortgage_paid']}")
+
+
 
